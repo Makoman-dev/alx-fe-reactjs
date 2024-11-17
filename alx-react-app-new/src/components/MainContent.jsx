309727c5edaed389d794b0d83552b0f2dd4ae31b
Makoman <elgamlahmed@gmail.com>
2024-11-17 07:50:47 +0200

    playing with inline styling in jsx

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+
+
+function MainContent() {
+    return (
+        <main>
+            <p style={{ border: '1px solid gray', padding: '10px', margin: '10px', color: 'blue', font: '24px'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;

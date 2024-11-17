7534699697d381e71cc2526546e75fa0cf931e31
Makoman <elgamlahmed@gmail.com>
2024-11-17 08:33:50 +0200

    rearranging files

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -8,8 +8,8 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
 import Counter from './components/Counter';
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext'; // Import the UserContext
+import ProfilePage from './components/ProfilePage';
+import UserContext from './components/UserContext'; // Import the UserContext
 
 function App() {
   const [count, setCount] = useState(0);
--- alx-react-app-new/src/components/ProfilePage.jsx
+++ alx-react-app-new/src/components/ProfilePage.jsx
@@ -3,6 +3,7 @@ import UserContext from './UserContext';
 
 function ProfilePage() {
   return <UserInfo />;
+  return <UserContext />;
 }
 
 export default ProfilePage;

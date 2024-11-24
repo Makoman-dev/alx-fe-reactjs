bfcec9d96f4bb44b8100a74c7baba2d4a2bdc5d6
Makoman <elgamlahmed@gmail.com>
2024-11-25 00:47:02 +0200

    sigh

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -11,17 +11,20 @@ describe('TodoList', () => {
     expect(todoItems).toHaveLength(3);
   });
 
-  it('adds a new todo', () => {
+  it('adds a new todo', async () => {
     render(<TodoList />);
     const input = screen.getByPlaceholderText('Add a todo');
     const addButton = screen.getByRole('button', { name: 'Add' });
-
+  
     userEvent.type(input, 'Test Todo');
     userEvent.click(addButton);
-
-    const todoItems = screen.getAllByRole('listitem');
-    expect(todoItems).toHaveLength(4);
-    expect(todoItems[3]).toHaveTextContent('Test Todo');
+  
+    // Wait for the component to re-render after state update
+    await waitFor(() => {
+      const todoItems = screen.getAllByRole('listitem');
+      expect(todoItems).toHaveLength(4);
+      expect(todoItems[3]).toHaveTextContent('Test Todo');
+    });
   });
 
   it('toggles a todo', () => {

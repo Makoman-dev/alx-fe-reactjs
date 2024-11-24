0152f4385ae13215ee731317fcb0ac615841e856
Makoman <elgamlahmed@gmail.com>
2024-11-25 00:18:43 +0200

    yeah

---
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+const ProtectedRoute = ({ isAuthenticated }) => {
+  return isAuthenticated ? <Outlet /> : <Navigate   
+ to="/" />;
+<Route path="/profile" element={
+  <ProtectedRoute isAuthenticated={true}>
+    <Profile />
+  </ProtectedRoute>
+}>
+  {/* ... Nested routes */}
+</Route>
+};

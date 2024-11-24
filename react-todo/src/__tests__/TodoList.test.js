6cb4b12bc2f3bfda206dd4351c13d76fa89d1b8a
Makoman <elgamlahmed@gmail.com>
2024-11-25 00:44:53 +0200

    wrapping up

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,42 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import TodoList from   
+ './TodoList';
+
+describe('TodoList', () => {
+  it('renders initial todos',   
+ () => {
+    render(<TodoList />);
+    const todoItems = screen.getAllByRole('listitem');
+    expect(todoItems).toHaveLength(3);
+  });
+
+  it('adds a new todo', () => {
+    render(<TodoList />);
+    const input = screen.getByPlaceholderText('Add a todo');
+    const addButton = screen.getByRole('button', { name: 'Add' });
+
+    userEvent.type(input, 'Test Todo');
+    userEvent.click(addButton);
+
+    const todoItems = screen.getAllByRole('listitem');
+    expect(todoItems).toHaveLength(4);
+    expect(todoItems[3]).toHaveTextContent('Test Todo');
+  });
+
+  it('toggles a todo', () => {
+    render(<TodoList />);
+    const firstTodoCheckbox = screen.getAllByRole('checkbox')[0];
+    userEvent.click(firstTodoCheckbox);
+    const firstTodo = screen.getAllByRole('listitem')[0];
+    expect(firstTodo).toHaveStyle({ textDecoration: 'line-through' });
+  });
+
+  it('deletes a todo', () => {
+    render(<TodoList />);
+    const firstTodoDeleteButton = screen.getAllByRole('button')[0];
+    userEvent.click(firstTodoDeleteButton);
+    const todoItems = screen.getAllByRole('listitem');
+    expect(todoItems).toHaveLength(2);
+  });
+});

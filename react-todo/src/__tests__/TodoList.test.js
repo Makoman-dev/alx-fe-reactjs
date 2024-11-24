21327c72a01d02147b4f5de0b2fbd320418d160d
Makoman <elgamlahmed@gmail.com>
2024-11-25 00:49:21 +0200

    did i debug it well? :D

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,7 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import TodoList from   
- './TodoList';
+import TodoList from './TodoList';
 
 describe('TodoList', () => {
   it('renders initial todos',   
